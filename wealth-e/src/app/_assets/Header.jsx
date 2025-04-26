166d8e6eb75a62733b786b8ab962b19ea93a9a06
fahim-ysr <fyasir@mun.ca>
2025-04-26 20:42:20 -0230

    Changed page title and description | Installed shadcn buttons | Installed Clerk for authentication

---
+++ wealth-e/src/app/_assets/Header.jsx
@@ -0,0 +1,7 @@
+// Forces the component to be a client component and avoid errors
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useUser, UserButton } from "@clerk/nextjs";
